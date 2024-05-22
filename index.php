4376bbd69ee5649aff7f765801f5e5ed99b2b6a5
Andrew MacLean <andrewdmaclean@gmail.com>
2024-05-22 22:03:51 +0000

    Move Modules to Individual Repos

--- index.php
+++ index.php
@@ -5,10 +5,14 @@ require __DIR__ . '/vendor/autoload.php';
 use Leaf\Blade;
 use DevCycle\DevCycle;
 use OneSignal\OneSignal;
+use Dotenv\Dotenv; 
+
+$dotenv = Dotenv::createImmutable(__DIR__); 
+$dotenv->load();
 
 $blade = new Blade('views', 'storage/cache');
-$dc = new DevCycle();
-$os = new OneSignal();
+$dc = new DevCycle($_ENV['DEVCYCLE_SERVER_SDK_KEY']);
+$os = new OneSignal($_ENV['REST_API_KEY'], $_ENV['USER_AUTH_KEY']);
 
 app()->get('/', function () use ($blade, $os) {
   echo DevCycle::ping();
