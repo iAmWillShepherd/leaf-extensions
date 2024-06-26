a637e520299cce9271612b1ab8bd81b508fd4ee9
William Shepherd <williamrshepherd@gmail.com>
2024-06-26 17:36:31 -0500

    Feat: send notification

--- index.php
+++ index.php
@@ -5,9 +5,11 @@ require __DIR__ . '/vendor/autoload.php';
 use Leaf\Blade;
 use DevCycle\DevCycle;
 use OneSignal\OneSignal;
-use Dotenv\Dotenv; 
+use Dotenv\Dotenv;
+use onesignal\client\model\Notification;
+use onesignal\client\model\StringMap;
 
-$dotenv = Dotenv::createImmutable(__DIR__); 
+$dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
 
 $blade = new Blade('views', 'storage/cache');
@@ -15,10 +17,24 @@ $dc = new DevCycle($_ENV['DEVCYCLE_SERVER_SDK_KEY']);
 $os = new OneSignal($_ENV['REST_API_KEY'], $_ENV['USER_AUTH_KEY']);
 
 app()->get('/', function () use ($blade, $os) {
-  echo DevCycle::ping();
-  echo OneSignal::ping();
-  $apps = $os->viewApps();
-  echo $blade->make('index', ['name' => 'DevCycle & OneSignal', 'apps' => $apps])->render();
+ $apps = $os->viewApps();
+ echo $blade->make('index', ['name' => 'DevCycle & OneSignal', 'apps' => $apps])->render();
+});
+
+app()->post('/test', function () use ($os) {
+    $appId = $_POST["appId"];
+    $message = $_POST["message"];
+    $notification = new Notification();
+    $notification->setAppId($appId);
+    $m = new StringMap();
+    $m->setEn($message);
+    $notification->setContents($m);
+
+    $notification->setIncludedSegments(['Total Subscriptions']);
+
+    $client = $os->getClient();
+    $client->createNotification($notification);
+
 });
 
 app()->run();
--- views/index.blade.php
+++ views/index.blade.php
@@ -7,12 +7,13 @@
 
 <body>
   <div class="container">{{ $name }}</div>
-  <form id="submit">
-    <select id="appId">
+  <form method="POST" action="/test" id="submit">
+    <select id="appId" name="appId">
       @foreach($apps as $app)
         <option value="{{ $app[1] }}">{{ $app[0] }}</option>
       @endforeach
     </select>
+    <textarea name="message" id="message"/></textarea>
     <button type="submit">Submit</button>
 </form>
 </body>
